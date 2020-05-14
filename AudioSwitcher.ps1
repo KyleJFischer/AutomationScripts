8b3b6ad4d01c271f06b0f491b5cdf89bc75e3ed5
Kyle Fischer <kylefischer1995@gmail.com>
2020-05-13 20:36:03 -0400

    Added Audio Switcher Script

---
+++ AudioSwitcher.ps1
@@ -0,0 +1,17 @@
+﻿$curSpeaker = Get-AudioDevice -Playback
+
+$listeningThruSpeakers = ($curSpeaker.Name -Match "Realtek")
+$listOfDevices = (Get-AudioDevice -List)
+
+if ($listeningThruSpeakers)
+{
+    echo "Switching to Headphones"
+    $newSpeaker = ($listOfDevices | Where-Object { $_.Name -Match "(HD 4.50BTNC Stereo)" -and $_.Name -notmatch "2-" })[0]
+
+} else {
+    echo "Switching to Speakers"
+    $newSpeaker = ($listOfDevices | Where-Object { $_.Name -Match "Realtek" })[0]
+}
+
+
+Set-AudioDevice -Index $newSpeaker.Index
